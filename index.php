f8a96d64dd3b22c945cb7c351d60619aff1395bd
kasolimando <130808675+kasolimando@users.noreply.github.com>
2023-07-24 19:17:42 -0400

    fix de pagina inicial (#2)

---
+++ index.php
@@ -0,0 +1,20 @@
+<?php
+	require_once 'layout/header.php';
+
+?>
+
+
+<main>
+		<div class="main-index">
+			<img  data-aos="fade-right" class="img-portada" src="img/portada.jpg">
+			<p data-aos="fade-left" class="text">Ayudar a cada niño a encontrar y seguir su mejor camino de aprendizaje.	</p>
+		</div>
+</main>
+
+<?php
+
+	require_once 'layout/footer.php';
+
+?>
+
+
