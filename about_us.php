04a68594fb689123cb78f6fad8cb71ca68dc3961
kasolimando <130808675+kasolimando@users.noreply.github.com>
2023-07-24 19:21:16 -0400

    Feat/about (#4)

---
+++ about_us.php
@@ -0,0 +1,50 @@
+<?php
+
+	require_once 'layout/header.php';
+
+?>
+
+
+<main class="main-quienes">
+	<section class="container-us">
+		<div class="values" data-aos="fade-right">
+			<div class="div-text">
+				<h3 class="title-us">VALORES</h3>
+				<h5 class="title2-us">PERSONALES</h5>
+				<p class="text-us">Compromiso, Respeto, Responsabilidad, Humildad, Sinceridad, Asertividad, Amor, Amistad.<p>
+				<h5 class="title2-us">SOCIALES</h5>
+				<p class="text-us">Trabajo, Unión, Solidaridad, Tolerancia, Empatía, Equidad, Constancia, Cooperación.<p>
+			</div>
+			<div>
+				<img class="img-us" src="img/valores.jpg">
+			</div>
+		</div>
+	</section>
+	<section class="container-propo">
+		<div class="item-uno">
+			<h3 class="title-us" data-aos="zoom-in">PROPOSITO</h3>
+		</div>
+		<div class="item-dos" data-aos="fade-right">
+			<p class="text-about">Garantizar desde la concepción Humanista Social, atención educativa integral a niños niñas y jóvenes con necesidades educativas especiales y/o discapacidad cognitiva con edades comprendidas entre 3 y 15 años, con el objeto de lograr su prosecución escolar e integración escolar e integración laboral y social a través de los diseños curriculares de educación inicial y educación primaria (primera etapa) mediante métodos estrategias y personal especializado en el marco de la participación activa y corresponsable de padres, madres, representante y comunidad para la formación de ciudadanos comprometidos, responsables, solidarios con capacidad de reflexión y sentido de justicia social identificado con los valores y tradiciones de la patria.</p>
+		</div>
+	</section>
+	<section class="container-alumnos">
+		<div class="info" data-aos="flip-left">
+			<h5 class="title2-us">TOTAL DE PERSONAS EN LA INSTITUCIÓN</h5>
+			<h5 class="title3-us"> 44 PERSONAL</h5>
+			<h5 class="title3-us"> 35 ALUMNOS</h5>
+			<h5 class="title3-us"> 28 ALUMNAS</h5>
+		</div>
+		<div class="img-alumnos">
+			<img class="img-alumnos" src="img/about-us2.jpg">
+		</div>
+	</section>
+</main>
+
+<?php
+
+	require_once 'layout/footer.php';
+
+?>
+
+
