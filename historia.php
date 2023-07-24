3d594f95f41a76899937bca99d3eee215dc40fa8
kasolimando <130808675+kasolimando@users.noreply.github.com>
2023-07-24 19:15:39 -0400

    Pagina de historia (#1)
    Pag de historia de la institución

---
+++ historia.php
@@ -0,0 +1,82 @@
+<?php
+
+	require_once 'layout/header.php';
+
+?>
+
+
+<main>
+<h1 data-aos="fade-right">HISTORIA I.E.E.B. “MODELO DEL SUR”, <span data-aos="fade-left">1975–2023</span></h1>
+<div class="flex-parent" data-aos="flip-down">
+	<div class="input-flex-container">
+		<input type="radio" name="timeline-dot" data-description="1910">
+		<div class="dot-info" data-description="1910">
+			<span class="year">1975</span>
+			<span class="label">Fundación</span>
+		</div>
+		<input type="radio" name="timeline-dot" data-description="1920">
+		<div class="dot-info" data-description="1920">
+			<span class="year">1999</span>
+			<span class="label">Sede Propia</span>
+		</div>
+		<input type="radio" name="timeline-dot" data-description="1930" checked>
+		<div class="dot-info" data-description="1930">
+			<span class="year">2000</span>
+			<span class="label">Registro</span>
+		</div>
+		<input type="radio" name="timeline-dot" data-description="1940">
+		<div class="dot-info" data-description="1940">
+			<span class="year">2001</span>
+			<span class="label">Lineamientos Establecidos</span>
+		</div>
+		<input type="radio" name="timeline-dot" data-description="1950">
+		<div class="dot-info" data-description="1950">
+			<span class="year">2006</span>
+			<span class="label">Dos turnos</span>
+		</div>
+		<input type="radio" name="timeline-dot" data-description="1960">
+		<div class="dot-info" data-description="1960">
+			<span class="year">2007</span>
+			<span class="label">Programa de Educación Bolivariana</span>
+		</div>
+		<input type="radio" name="timeline-dot" data-description="1970">
+		<div class="dot-info" data-description="1970">
+			<span class="year">2009</span>
+			<span class="label">Currículum Bolivariano</span>
+		</div>
+		<input type="radio" name="timeline-dot" data-description="1980">
+		<div class="dot-info" data-description="1980">
+			<span class="year">2012</span>
+			<span class="label">Abolición de la Resolución Nº 751</span>
+		</div>
+		<input type="radio" name="timeline-dot" data-description="1990">
+		<div class="dot-info" data-description="1990">
+			<span class="year">2019</span>
+			<span class="label">COVID-19</span>
+		</div>
+		<input type="radio" name="timeline-dot" data-description="2000">
+		<div class="dot-info" data-description="2000">
+			<span class="year">2023</span>
+			<span class="label">Horario Actual</span>
+		</div>
+		<div id="timeline-descriptions-wrapper">
+			<p data-description="1910">El Instituto de Educación Especial Bolivariano “Modelo del Sur”, es un Plantel Oficial de la Modalidad de Educación Especial, adscrito al Ministerio del Poder Popular para la Educación. Fundado en octubre de 1975.</p>
+			<p data-description="1920">Adquirida por La Fundación de Edificaciones y Dotación Educativa (FEDE) al cual se le realizaron adecuaciones para la misión escolar que se lleva a cabo.</p>
+			<p data-description="1930">Los Padres y Representantes se organizaron y registraron La Asociación Civil “Modelo del Sur”, atendiendo a lo establecido en la Constitución de la República Bolivariana de Venezuela, Ley Orgánica de Educación, Ley Orgánica de Protección del Niño y del Adolescentes, Resolución Nº 751.</p>
+			<p data-description="1940">Se inició el proceso de estudio y análisis de los Diseños Curriculares de los niveles Educación Inicial y Educación Básica.</p>
+			<p data-description="1950">Se trabajaba en dos turnos (de 7:00 a.m. a 12:00 m. y de 1:00 p.m. a 6:00 p.m.).</p>
+			<p data-description="1960">Fue incorporado al programa de Educación Bolivariana.</p>
+			<p data-description="1970">Se incluye el currículum Bolivariano para realizar las adaptaciones curriculares pertinentes</p>
+			<p data-description="1980">Es abolida esta resolución quedando la 058 con todas sus aplicaciones dándole inicio a la participación de la comunidad (consejo educativo).</p>
+			<p data-description="1990">Se detienen las actividades escolares por motivo de la pandemia causado por el virus COVID-19.</p>
+			<p data-description="2000">Funcionando actualmente en un horario integral de 8 horas. de 7:00 am a 3:00pm.</p>
+		</div>
+	</div>
+</div>
+</main>
+
+<?php
+
+	require_once 'layout/footer.php';
+
+?>
