0231c35b4de6fdf561c17d7e3b5bc8508a8fa54e
kasolimando <kasolimando@gmail.com>
2023-07-24 19:17:58 -0400

    pagina de informacion institucional

---
+++ contact.php
@@ -0,0 +1,48 @@
+<?php
+	require_once 'layout/header.php';
+
+?>
+
+
+<main>
+		<div class="main-contact">
+			<div class="img-contact" data-aos="flip-left">
+				<img class="img-contact" src="img/contact.jpg">
+			</div>
+			<div class="papper" draggable="true" data-aos="flip-right">
+				<h5 class="title-contact">Horario Laboral</h5>
+				<p class="text-contact">Semana  Lun-Vie</p>
+				<p class="text-contact">Escolar  8:00am - 12:00pm</p>
+				<p class="text-contact">Administrativo 8:00am - 1:00pm</p>
+			</div>	
+		</div>
+		<div class="info-contact">
+			<div class="item-contact" data-aos="zoom-in">
+				<img class="img-info" alt="centered image" src="img/telefono.png"/>
+				<h5 class="texto-item-contact">NÚMERO DE CONTACTO</h5>
+				<p class="texto-item-contact">+58 212-661.20.54</p>
+			</div>
+			<div class="item-contact" data-aos="zoom-in">
+				<img class="img-info" src="img/maps-and-location.png"/>
+				<h5 class="texto-item-contact">UBICACIÓN</h5>
+				<p class="texto-item-contact">Urbanización “Los Chaguaramos” calle Madariaga Quinta Santa Eduvigis</p>
+				<iframe class="map" src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3923.2439693240035!2d-66.8874021!3d10.4814239!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x8c2a5f227745381b%3A0xd3e2b724fa5a93b2!2sIEEB%20MODELO%20DEL%20SUR!5e0!3m2!1ses-419!2sve!4v1680650693181!5m2!1ses-419!2sve" width="45%" height="45%" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
+			</div>
+			<div class="item-contact" data-aos="zoom-in">
+				<img class="img-info" src="img/correo-electronico-vacio.png"/>
+				<h5 class="texto-item-contact">CORREO</h5>
+				<p class="texto-item-contact">info@mail.com</p>
+			</div>
+			<div class="item-contact" data-aos="zoom-in">
+				<img class="img-info" src="img/correo-electronico-vacio.png"/>
+				<h5 class="texto-item-contact">INFORMACION</h5>
+				<p class="texto-item-contact">INFORMACION</p>
+			</div>
+		</div>
+</main>
+
+<?php
+
+	require_once 'layout/footer.php';
+
+?>
